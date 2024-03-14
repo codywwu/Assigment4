14b71d11cf3a5ae02bc8e811b469c3d8e3929c39
wwu <wucody59@gmail.com>
2024-03-14 17:35:54 -0400

    minor fix

--- src/models/Model.java
+++ src/models/Model.java
@@ -215,7 +215,7 @@ public class Model implements ModelInterface{
    * @return true if existed.
    */
   public boolean checkFileExists(String inFile) {
-    File folder = new File("../InputData/");
+    File folder = new File("./InputData/");
 
 //    return folder.exists();
     File[] files = folder.listFiles();
